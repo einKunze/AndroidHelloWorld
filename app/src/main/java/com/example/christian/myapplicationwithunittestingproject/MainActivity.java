c55900903a8ec4ea5616ce99ea5dc57af8c6d6ea
ckdev42 <c_kunze@gmx.de>
2017-01-17 00:39:35 +0100

    yeahooo

---
+++ app/src/main/java/com/example/christian/myapplicationwithunittestingproject/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.christian.myapplicationwithunittestingproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Vector;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        doSomething();
+    }
+
+    private void doSomething() {
+
+        Vector v = new Vector();
+        v.add("Hello");
+
+        if (v.contains("Hello")) {
+
+        } else {
+
+        }
+    }
+}
